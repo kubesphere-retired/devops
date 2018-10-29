62eebc0ce7c4835f275c72181d4c831252d310b0
runzexia <root@junwuhui.cn>
2018-10-29 16:05:52 +0800

    move credential table to v0.2

--- pkg/db/schema/devops/V0_1__init.sql
+++ pkg/db/schema/devops/V0_1__init.sql
@@ -21,11 +21,3 @@ CREATE TABLE `project_membership` (
   PRIMARY KEY (`username`, `project_id`)
 );
 
-CREATE TABLE `project_credential` (
-  `project_id`    VARCHAR(50)  NOT NULL,
-  `credential_id` VARCHAR(255) NOT NULL,
-  `domain`        VARCHAR(255) NOT NULL,
-  `creator`       VARCHAR(50)  NOT NULL,
-  `create_time`   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`project_id`, `credential_id`, `domain`)
-);
